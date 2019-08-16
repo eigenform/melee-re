fe77f965094479c31857a91efde372d72b175b64
meta <hosaka-corp@users.noreply.github.com>
2019-08-16 15:46:55 -0500

    finally writes arbitrary, persistent text!!

--- asm/debug_text/main.asm
+++ asm/debug_text/main.asm
@@ -1,4 +1,4 @@
-# Write this on 0x801a4d98, inside `EngineUpdate()`; this will clobber a 
+# Write this on 0x801a4d98, inside `EngineUpdate()` - this will clobber a 
 # `bl USBScreenshot_InitMCC`, which is effectively useless to everyone, unless 
 # someone is actually using USB2EXI. Runs every iteration of the engine loop.
 
@@ -8,84 +8,151 @@ __start:
 	backup
 
 	# If varAllocated is zero, we haven't allocated objects yet
-	bl varAllocated
-	mflr r4
+	load_rt r4, varAllocated
 	lwz r3, 0x0(r4)
 	cmpwi r3, 0
 
 	# If we've allocated objects, check to see if we can draw 
 	bne checkDrawing
 
-	# If we haven't allocated objects, allocate them and exit.
+__allocateMemory:
 	# This is literally just the allocation from Develop Mode bonuses,
 	# and this corresponds to the arguments to drawing functions.
-	branchl r11, 0x80228cf4
+	#branchl r11, 0x80228cf4
+
+	# Allocate some memory for the string to display and save the pointer.
+	# Must be at least `TextData->max_rows * TextData->max_width * 2`
+	# (this how `DevelopMode_Text_Erase()` calculates string size?)
+	li r3, 0x500
+	branchl r11, _HSD_MemAlloc
+	load_rt r4, stringPtr
+	stw r3, 0x0(r4)
+
+	# Instantiate a new TextData object, then save the pointer to it
+	# r3=id?, r4=x_off, r5=y_off, r6=max_width, r7=max_row, r8=string ptr
+	li r3, 1	
+	li r4, 0x10
+	li r5, 0x10
+	li r6, 32
+	li r7, 2
+	load_rt r8, stringPtr
+	lwz r8, 0(r8)
+	branchl r11, TextData_Create
+	load_rt r4, textObjPtr
+	stw r3, 0(r4)
+
+	# Pass textObjPtr to this function to enable it?
+	mr r4, r3
+	branchl r11, TextData_Init
+
+	# Set background color
+	load_rt r4, textObjPtr
+	lwz r3, 0(r4)
+	load_rt r4, backgroundColor
+	branchl r11, TextData_SetBackgroundColor
+
+	# Unhide the object
+	load_rt r4, textObjPtr
+	lwz r3, 0(r4)
+	branchl r11, TextData_Unhide
 
 	# Set varAllocated to non-zero and exit
 	li r3, 1
+	load_rt r4, varAllocated
 	stw r3, 0x0(r4)
 	b __exit
 
 # First, we need to check to see if drawing has been disabled. If disabled 
 # (i.e. after a scene transition), just re-enable it by writing the pointer.
 checkDrawing:
-
 	# r3 typically holds a pointer main develop text object
-	load r3, 0x804d6e18
+	#load r3, 0x804d6e18
 
 	# r4 is typically the address of the main object
-	load r4, 0x804a1fd8
+	#load r4, 0x804a1fd8
 
 	# If the pointer is zero, it means drawing is disabled, so re-enable 
 	# it and just exit this function for now
-	lwz r5, 0x0(r3)
-	cmpwi r5, 0
-	beq __enableDrawing
+	#lwz r5, 0x0(r3)
+	#cmpwi r5, 0
+	#beq __enableDrawing
+
+	# If this pointer is non-null, it's probably the pointer to our
+	# object, so we can continue updating our objects
+	load r3, 0x804d6e18
+	lwz r3, 0(r3)
+	cmpwi r3, 0
+	bne __drawText
+
+	# Otherwise, if the pointer was null, re-initialize our object
+	# and exit
+	load_rt r4, textObjPtr
+	lwz r4, 0(r4)
+	branchl r11, TextData_Init
+	b __exit
+
+
 	
-# If drawing is enabled, we can mutuate the develop text object/s.
+# If drawing is enabled, we're free to mutate our text objects
 __drawText:
-	load r3, 0x804a1fd8
-	load r4, 0x803f0000
+	#load r3, 0x804a1fd8
+	load_rt r4, textObjPtr
+	lwz r3, 0(r4)
 	branchl r11, DevelopMode_Text_Erase
 
-	load r3, 0x804a1fd8
+	#load r3, 0x804a1fd8
+	load_rt r4, textObjPtr
+	lwz r3, 0(r4)
 	li r4, 0
 	li r5, 0
 	branchl r11, DevelopMode_Text_ResetCursor
 
-	load r3, 0x804a1fd8
-	bl string1Ptr
-	mflr r4
+	#load r3, 0x804a1fd8
+	load_rt r4, textObjPtr
+	lwz r3, 0(r4)
+	load_rt r4, fmtstring1
 	branchl r11, DevelopMode_Text_Display
 
-	load r3, 0x804a1fd8
-	bl string2Ptr
-	mflr r4
+	#load r3, 0x804a1fd8
+	load_rt r4, textObjPtr
+	lwz r3, 0(r4)
+	load_rt r4, fmtstring2
 	branchl r11, DevelopMode_Text_Display
 
 	b __exit
 
-# Local storage - keep track of when we've allocated text objects. God only 
-# knows what happens if you branch into 0x80228cf4 somewhere else (this is
-# specific to the Develop Mode Bonuses Text with 'B + DpadLeft' I think ...)
+
+# Local storage - stuff we allocate for our objects
 varAllocated:
 	blrl
 	.word 0x00000000
 	.align 4
+textObjPtr:
+	blrl
+	.word 0x00000000
+	.align 4
+stringPtr:
+	blrl
+	.word 0x00000000
+	.align 4
+backgroundColor:
+	blrl
+	.word 0x00000000
+	.align 4
 
-# Local storage for format strings
-string1Ptr:
+# Local storage - format strings for rendering text
+fmtstring1:
 	blrl
 	.string "Hello, world!\n"
 	.align 4
-string2Ptr:
+fmtstring2:
 	blrl
 	.string "This is a test!\n"
 	.align 4
 
 # Re-enable develop mode text drawing, then fall-though to the end
-__enableDrawing:
-	stw r4, 0x0(r3)
+#__enableDrawing:
+#	stw r4, 0x0(r3)
 
 __exit:
 	restore
